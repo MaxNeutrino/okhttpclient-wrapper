8b46732b59b8fd3c2ef181653dfaefbc8a3f4221
Maxim Stepovoy <asumnasumn@gmail.com>
2017-09-06 17:06:20 +0300

    init

---
+++ src/main/kotlin/neutrino/project/clientwrapper/Client.kt
@@ -0,0 +1,152 @@
+package neutrino.project.clientwrapper
+
+import okhttp3.*
+import java.io.File
+import java.net.CookieManager
+import java.net.CookiePolicy
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+
+class Client {
+
+	val baseUrl: String
+
+	val coreClient: OkHttpClient
+
+	var cookieManager: CookieManager? = null
+
+	constructor(baseUrl: String) {
+		this.baseUrl = baseUrl
+		this.coreClient = createDefault()
+	}
+
+	constructor(baseUrl: String, coreClient: OkHttpClient) {
+		this.baseUrl = baseUrl
+		this.coreClient = coreClient
+	}
+
+	fun newRequestBuilder(): RequestBuilder {
+		return RequestBuilder(Request.Builder())
+	}
+
+	fun newCustomClientBuilder(clientBaseUrl: String): CustomClientBuilder {
+		return CustomClientBuilder(clientBaseUrl)
+	}
+
+	inner class RequestBuilder(private val requestBuilder: Request.Builder) {
+
+		fun url(url: String): RequestBuilder {
+			requestBuilder.url("$baseUrl$url")
+			return this
+		}
+
+		fun addHeader(name: String, value: String): RequestBuilder {
+			requestBuilder.header(name, value)
+			return this
+		}
+
+		fun get(): RequestBuilder {
+			requestBuilder.get()
+			return this
+		}
+
+		fun post(params: Map<String, String>): RequestBuilder {
+			val requestBodyBuilder = FormBody.Builder()
+			params.forEach { requestBodyBuilder.add(it.key, it.value) }
+			val requestBody = requestBodyBuilder.build()
+
+			requestBuilder.post(requestBody)
+			return this
+		}
+
+		fun execute(): Optional<Response> {
+			val request: Request? = requestBuilder.build()
+			val response = coreClient
+					.newCall(request)
+					.execute()
+
+			return Optional.ofNullable(response)
+		}
+
+		fun executeAndGetBody(): Optional<String> {
+			val request: Request? = requestBuilder.build()
+			val response = coreClient.newCall(request).execute()
+			val body = response.body()?.string()
+			response.close()
+
+			return Optional.ofNullable(body)
+		}
+	}
+
+	inner class CustomClientBuilder(private val clientBaseUrl: String) {
+
+		private val clientBuilder = OkHttpClient.Builder()
+
+		fun createSimple(baseUrl: String): Client {
+			val coreClient: OkHttpClient = clientBuilder.build()
+			return Client(baseUrl, coreClient)
+		}
+
+		fun withAutoSaveCookies(): CustomClientBuilder {
+			clientBuilder.cookieJar(JavaNetCookieJar(createCookieManager()))
+			return this
+		}
+
+		fun withCache(dirName: String): CustomClientBuilder {
+			clientBuilder.cache(getCache(dirName))
+			return this
+		}
+
+		fun followRedirects(isFolow: Boolean): CustomClientBuilder {
+			clientBuilder.followRedirects(isFolow)
+			return this
+		}
+
+		fun withTimout(timeout: Long, timeUnit: TimeUnit): CustomClientBuilder {
+			clientBuilder.connectTimeout(timeout, timeUnit)
+					.readTimeout(timeout, timeUnit)
+					.writeTimeout(timeout, timeUnit)
+			return this
+		}
+
+		fun withConnectionPool(pool: ConnectionPool): CustomClientBuilder {
+			clientBuilder.connectionPool(pool)
+			return this
+		}
+
+		fun build(): Client {
+			return Client(clientBaseUrl, clientBuilder.build())
+		}
+
+		fun buildWithCustom(okHttpClient: OkHttpClient): Client {
+			return Client(clientBaseUrl, okHttpClient)
+		}
+	}
+
+	private fun createDefault(): OkHttpClient {
+		return OkHttpClient.Builder()
+				.cookieJar(JavaNetCookieJar(cookieManager))
+				.cache(getCache("victoria"))
+				.followRedirects(true)
+				.connectTimeout(2, TimeUnit.MINUTES)
+				.readTimeout(2, TimeUnit.MINUTES)
+				.writeTimeout(2, TimeUnit.MINUTES)
+				.connectionPool(ConnectionPool(15, 5, TimeUnit.MINUTES))
+				.build()
+	}
+
+	private fun getCache(child: String): Cache {
+		val cacheDir = File(System.getProperty("java.io.tmpdir"), child)
+		return Cache(cacheDir, 1024)
+	}
+
+	private fun createCookieManager(): CookieManager {
+
+		val cookieManager = CookieManager()
+		cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
+
+		this.cookieManager = cookieManager
+		return cookieManager
+	}
+}
---
+++ src/main/kotlin/neutrino/project/clientwrapper/CookieFileStore.kt
@@ -0,0 +1,63 @@
+package neutrino.project.clientwrapper
+
+import com.google.gson.GsonBuilder
+import java.net.HttpCookie
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+import java.net.CookieStore
+import java.net.URI
+
+
+object CookieFileStore {
+
+	private val jsonType = object : TypeToken<List<HttpCookie>>() {}.type
+
+	fun saveCookie(cookies: List<HttpCookie>?, saveFile: File) {
+		FileWriter(saveFile).use { writer ->
+			val gson = GsonBuilder().create()
+			gson.toJson(cookies, writer)
+		}
+	}
+
+	fun saveCookie(cookies: List<HttpCookie>?, saveFile: String) {
+		saveCookie(cookies, File(saveFile))
+	}
+
+	fun saveCookie(client: Client, saveFile: File) {
+		val cookies = client.cookieManager?.cookieStore?.cookies
+		saveCookie(cookies, saveFile)
+	}
+
+	fun saveCookie(client: Client, saveFile: String) {
+		val cookies = client.cookieManager?.cookieStore?.cookies
+		saveCookie(cookies, saveFile)
+	}
+
+	fun restoreCookie(saveFile: File): List<HttpCookie>? {
+		val cookies: MutableList<HttpCookie> = mutableListOf()
+		JsonReader(FileReader(saveFile)).use { reader ->
+			val gson = GsonBuilder().create()
+			cookies.addAll(
+					gson.fromJson(reader, jsonType))
+		}
+		return cookies
+	}
+
+	fun restoreCookie(saveFile: String): List<HttpCookie>? {
+		return restoreCookie(File(saveFile))
+	}
+
+	fun restoreCookie(client: Client, saveFile: File) {
+		val store = client.cookieManager?.cookieStore
+		restoreCookie(saveFile)?.forEach { cookie ->
+			store?.add(URI(client.baseUrl), cookie)
+		}
+	}
+
+	fun restoreCookie(client: Client, saveFile: String) {
+		restoreCookie(client, File(saveFile))
+	}
+}
